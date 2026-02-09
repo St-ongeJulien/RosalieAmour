c5d3df9aff03d4a9b4a8eb9267ef1a15eb142e72
St-ongeJulien <56926020+St-ongeJulien@users.noreply.github.com>
2026-02-09 05:00:37 -0500

    Update vite.config.js

--- rosaliemyvalentine/vite.config.js
+++ rosaliemyvalentine/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlify from "@netlify/vite-plugin";
+
 
 export default defineConfig({
   plugins: [react()],
+  plugins: [netlify()],
 })
+
+
